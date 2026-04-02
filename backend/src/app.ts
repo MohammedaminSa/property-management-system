3f4b5472d4ee402887e9bc0e087b6af10cab2542
Mohammed Amin <mhmmdmnsdk@gmail.com>
2026-04-02 17:42:19 +0300

    t

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -81,7 +81,7 @@ const PORT = process.env.PORT || 3000;
 // export default app
 // ---------- Start Server ----------
 app.listen(PORT as any, "0.0.0.0", () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
   console.log(`🚀 The new version is running`);
   console.log(`🚀 Friday 13 - hmmmm`);
   console.log(`📖 Swagger Docs in here http://localhost:${PORT}/api-docs`);
